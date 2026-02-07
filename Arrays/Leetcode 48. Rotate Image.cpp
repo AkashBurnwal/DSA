a20c5fcf33dabfcc2953a5d72008585a44222aee
AkashBurnwal <116300814+AkashBurnwal@users.noreply.github.com>
2026-02-07 23:35:39 +0530

    Create Leetcode 48. Rotate Image.cpp

---
+++ Arrays/Leetcode 48. Rotate Image.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int r = matrix[0].size();
+        int c = matrix.size();
+        for(int i =0;i <r ; i++)
+        {
+            for(int j = i ; j<c ; j++)
+            {
+                swap(matrix[i][j] , matrix[j][i]);
+            }
+        }
+        for(int i = 0 ; i<r; i++)
+        {
+            reverse(matrix[i].begin() , matrix[i].end());
+        }
+    }
+};
